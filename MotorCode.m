1115648fd80c614bd343fd17d6489147ac053104
moham <moham@momo>
2024-10-31 11:52:54 +0100

    \Update mOtorcode

--- MotorCode.m
+++ MotorCode.m
@@ -14,11 +14,12 @@ acceleration_voltage = max(Voltage); %The voltage that is used
 disp(acceleration_voltage)
 max_voltage = 3.3;            % Voltage level for PWM (3.3V)
 period = 0.01;         % The total period of PWM signal (s)
-frequency = 100;       % 1/period
+frequency = 10000;       % 1/period
 channelOut = 0; % Channel that outputs the PWM wave
 
 % Determing thew duty cycle in percentages
-duty_cycle = ;
+duty_cycle = 52.5
+
 
 % Generate the PWM signal based on the duty cycle
 AD2initPWM(hdwf,channelOut,duty_cycle,frequency);
@@ -32,7 +33,7 @@ pause(3)
 
 % open the solenoid
 AD2SetDigitalIO(hdwf,solenoid_pin_open)
-pause(0.6);
+pause(0.2);
 
 %close the solenoid
 AD2SetDigitalIO(hdwf,solenoid_pin_close);
